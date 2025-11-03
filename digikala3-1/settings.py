e15c6225fb95435574723e29d6be286023a88597
sepehr.sr <sepehrsarraf98@gmail.com>
2025-11-03 21:22:15 +0330

    Update settings.py

--- digikala3-1/settings.py
+++ digikala3-1/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-v=_c^)xb@ljtl_7*mbmnl*7go%cxcbu^%1qm8^8$1#7^8s*-r1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['digikala3-1.onrender.com','127.0.0.1:8000','localhost']
+ALLOWED_HOSTS = ['digikala3-1.onrender.com','127.0.0.1','localhost']
 
 
 
